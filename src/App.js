51e3ef496ec6e7306068477928e06332ebaba18f
xthiagosant <uzmaki_thiago@outlook.com>
2020-06-23 13:49:20 -0300

    criação de rotas - Entidades

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, BrowserRouter, Router, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Entidade from './pages/Entidade';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <h1>header...</h1>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/entidade' component={Entidade} />
         </Switch>
         <h1>footer...</h1>
       </div>    
---
+++ src/pages/Entidade.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Entidade = () => {
+    return(
+        <div>
+            <h1>Entidade</h1>
+        </div>
+    )
+}
+
+export default Entidade
